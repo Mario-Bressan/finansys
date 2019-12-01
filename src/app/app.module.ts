eb7f8abde9505fe4dd67d096ae79e513f671479f
Mario Bressan <mario.bressan@hotmail.com>
2019-12-01 20:31:05 -0300

    Rotas configuradas e in memory web api

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import {InMemoryDatabase} from './in-memory-database';
+import { HttpClientModule} from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    // ativação do in memory web api
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
